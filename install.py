57e563f6990c3ac52177bf0c636d84e3d8e51705
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-05 11:52:17 +0100

    Add files via upload

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+import sys
+os.system("clear")
+os.system("figlet U-danbaiwa")
+print("please wait...")
+os.system("cd /data/data/com.termux/files/home")
+os.system("pkg update -y")
+os.system("pkg install -y git")
+os.system("pkg install python2")
+os.system("pkg install figlet -y")
+print(cya+"\t=====COMPLETE INSTALL THANK YOU=====")
